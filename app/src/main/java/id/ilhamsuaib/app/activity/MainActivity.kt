32889c563976769d16ae21adbac98f1fa0b2c475
ilhamsuaib <ilhamsuaib10@gmail.com>
2018-12-05 22:38:13 +0700

    fragment dan bottom navigation

--- app/src/main/java/id/ilhamsuaib/app/activity/MainActivity.kt
+++ app/src/main/java/id/ilhamsuaib/app/activity/MainActivity.kt
@@ -1,8 +1,12 @@
 package id.ilhamsuaib.app.activity
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import id.ilhamsuaib.app.R
+import id.ilhamsuaib.app.fragment.MaterialFragment
+import id.ilhamsuaib.app.fragment.ProfileFragment
+import id.ilhamsuaib.app.fragment.StudentFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,26 @@ class MainActivity : AppCompatActivity() {
         //mengambil data dari intent
         val username: String = intent.getStringExtra(LoginActivity.USERNAME)
 
-        tvUseranme.text = username
+        setupView()
+    }
+
+    private fun setupView() {
+
+        setFragment(StudentFragment(), getString(R.string.student))
+        mainNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuNavStudent -> setFragment(StudentFragment(), getString(R.string.student))
+                R.id.menuNavMaterial -> setFragment(MaterialFragment(), getString(R.string.material))
+                R.id.menuNavProfile -> setFragment(ProfileFragment(), getString(R.string.profile))
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    private fun setFragment(frm: Fragment, title: String) {
+        this.title = title
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, frm)
+        transaction.commit()
     }
 }
